8e479fe0988109538152b0096027d5ef2031e5fd
avi-213 <avishmakar071@gmail.com>
2026-05-16 14:44:37 +0530

    Add files via upload

--- components/ProfitCurve.tsx
+++ components/ProfitCurve.tsx
@@ -1,38 +1,33 @@
 "use client";
 
 import {
-
   Chart as ChartJS,
-
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
   BarElement,
-
+  BarController,
+  LineController,
   Tooltip,
   Legend,
-
   Filler,
-
 } from "chart.js";
 
 import { Chart } from "react-chartjs-2";
 
 ChartJS.register(
-
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
   BarElement,
-
+  BarController,
+  LineController,
   Tooltip,
   Legend,
-
   Filler
 );
-
 type Match = {
 
   amount: number;
